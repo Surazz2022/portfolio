7e640d55392fdbd5f0bba8e4eca9a0bbc0251207
Surazz2022 <surz.khl49@gmail.com>
2025-05-13 12:10:39 +0545

    suraj

--- django_crud_project/urls.py
+++ django_crud_project/urls.py
@@ -23,6 +23,6 @@ from django.shortcuts import redirect
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('crudapp.urls')),
-    path('/about')
+    #path('/about')
     #path('', lambda request: redirect('item_list')),  # Redirect root to item_list
 ]
