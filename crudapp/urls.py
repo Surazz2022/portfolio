60b10967cbb0b66bd0df154888822908dd04daba
Surazz2022 <surz.khl49@gmail.com>
2025-05-13 11:57:24 +0545

    initial commit

---
+++ crudapp/urls.py
@@ -0,0 +1,13 @@
+# crudapp/urls.py
+
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    #path('', views.item_list, name='item_list'),
+    path('new/', views.item_create, name='item_create'),
+    path('<int:pk>/edit/', views.item_update, name='item_update'),
+    path('<int:pk>/delete/', views.item_delete, name='item_delete'),
+    path('', views.Suraj, name='Suraj'),
+
+]
